09d51aca2c1fcc23ea96af3a984b74c18423c470
Grzegorz Pokorski <mr.pokorski@gmail.com>
2021-11-04 19:55:58 +0100

    removing redundant imports from app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import car_data
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
